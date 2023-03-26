4290f920c7401d05809f56d4e698bfa4beae57bf
dmitry.yegorov <dmitry.yegorov@effective-soft.com>
2023-03-26 23:05:04 +0300

    init migration schemas

--- cmd/main.go
+++ cmd/main.go
@@ -7,9 +7,13 @@ import (
 	"github.com/DmitryYegorov/go-todo/pkg/handler"
 	"github.com/DmitryYegorov/go-todo/pkg/repository"
 	"github.com/DmitryYegorov/go-todo/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error init config: %s", err.Error())
+	}
 
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
@@ -17,7 +21,14 @@ func main() {
 	server := new(todo.Server)
 	handlers := handler.NewHandler(services)
 
-	if err := server.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := server.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error ocurred while running HTTP server %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigFile("config")
+
+	return viper.ReadInConfig()
 }
